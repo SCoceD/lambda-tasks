c95310f9a9f75b63b51392c2bb71e75a6ada7af2
APolieshchuk <anton2013ua@gmail.com>
2022-05-01 01:13:34 +0300

    Initial commit

---
+++ insta/instagram-giveaway.js
@@ -0,0 +1,84 @@
+const fsa = require("fs").promises;
+const fs = require("fs");
+
+const arrOfSetsFromAllFile = [];
+const setUniqNames = new Set();
+const uniqueValues = async () => {
+    const path = __dirname + '/files/';
+    const start = new Date().getTime();
+    let fileNames = fs.readdirSync(path);
+    let dataPromises = [];
+    for (let i = 0; i < fileNames.length; i++) {
+        // let data = fsa.readFileSync('/home/apolieshchuk/Downloads/lambda/files/out' + i + '.txt', 'utf8');
+        // data.split('\n').forEach(element => setUniqNames.add(element))
+        dataPromises.push(fsa.readFile(path + fileNames[i], 'utf8')
+            .then(data => data.split('\n').forEach(element => setUniqNames.add(element))
+            )
+        );
+    }
+    await Promise.all(dataPromises);
+    console.log('Уникальных словосочетаний: ' + setUniqNames.size)
+    console.log('Time of work: ' + (new Date().getTime() - start));
+}
+
+const existInAllFiles = async () => {
+    const path = __dirname + '/files/';
+    const start = new Date().getTime();
+    let fileNames = fs.readdirSync(path);
+    let twentyOrMore = 0;
+    await parseNames();
+    setUniqNames.forEach(elementUniq => {
+        let counter = 0;
+        arrOfSetsFromAllFile.forEach(elementInSets => {
+            if (elementInSets.has(elementUniq)) {
+                counter++;
+                if (counter === fileNames.length) {
+                    twentyOrMore++;
+                }
+            }
+        })
+    })
+    console.log('Словосочетаний, которые есть во всех 20 файлах: ' + twentyOrMore);
+    console.log('Time of work: ' + (new Date().getTime() - start));
+}
+
+const existInAtLeastTen = async () => {
+    const start = new Date().getTime();
+    let tenOrMore = 0;
+    await parseNames();
+    setUniqNames.forEach(elementUniq => {
+        let counter = 0;
+        arrOfSetsFromAllFile.forEach(elementInSets => {
+            if (elementInSets.has(elementUniq)) {
+                counter++;
+                if (counter === 10) {
+                    tenOrMore++;
+                }
+            }
+        })
+    })
+    console.log('Словосочетаний, которые есть, как минимум, в десяти файлах: ' + tenOrMore);
+    console.log('Time of work: ' + (new Date().getTime() - start));
+}
+
+async function parseNames() {
+    const path = __dirname + '/files/'
+    let fileNames = fs.readdirSync(path);
+    let dataPromises = [];
+    for (let i = 0; i < fileNames.length; i++) {
+        const setOfName = new Set();
+        dataPromises.push(fsa.readFile(path + fileNames[i], 'utf8')
+            .then(data => data.split('\n').forEach(element => {
+                setOfName.add(element);
+                setUniqNames.add(element);
+            })));
+        arrOfSetsFromAllFile.push(setOfName);
+    }
+    await Promise.all(dataPromises);
+}
+
+(async () => {
+    await uniqueValues()
+    await existInAllFiles();
+    await existInAtLeastTen();
+})()
